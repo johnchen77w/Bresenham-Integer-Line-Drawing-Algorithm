56d9991b6a803b8d56fd5819728f3509e0701ce5
John Chen <johnchen77w@gmail.com>
2020-01-21 00:30:11 -0500

    Add files via upload

--- graphicsWindow.py
+++ graphicsWindow.py
@@ -25,129 +25,50 @@ class graphicsWindow:
     # do we add a self in front? HUH
     def drawLine(self,p1,p2,color):
 
+        def bresenham_odd (x1, y1, dx, dy, inc_x, inc_y):
+            self.drawPixel((x1, y1), color)
+            p = 2 * dy - dx
+            for x in range(abs(dx)):
+                if p < 0:
+                    p = p + 2 * dy
+                else:
+                    p = p + 2 * dy - 2 * dx
+                    y1 += inc_y
+                x1 += inc_x
+                self.drawPixel((x1, y1), color)
+
+        def bresenham_even (x1, y1, dx, dy, inc_x, inc_y):
+            self.drawPixel((x1, y1), color)
+            p = 2 * dx - dy
+            for y in range(abs(dy)):
+                if p < 0:
+                    p = p + 2 * dx
+                else:
+                    p = p + 2 * dx - 2 * dy
+                    x1 += inc_x
+                y1 += inc_y
+                self.drawPixel((x1, y1), color)
+
         x1, y1 = p1
         x2, y2 = p2
-
         dx = x2 - x1
         dy = y2 - y1
 
-        try:
-            slope = dy/dx
-        except ZeroDivisionError:
-            slope = None
+        try: slope = dy/dx
+        except ZeroDivisionError: slope = None
         
-        if slope is None:
-            # vertical line
+        if slope is None:   # vertical line
             y = min(y1, y2)
-            for i in range(abs(dy)):
-                self.drawPixel((x1, y + i), color)
+            for i in range(abs(dy)): self.drawPixel((x1, y + i), color)
+        elif slope == 0:    # horizontal line
+            x = min(x1, x2)
+            for i in range(abs(dx)): self.drawPixel((x + i, y1), color)
         else:
-            # x > 0
-            if x1 < x2:
-                # Q1
-                if y1 < y2:
-                    # Octant 1
-                    if 0 <= slope and slope <= 1:
-                        self.drawPixel((x1, y1), color)
-                        pi = 2 * dy - dx
-                        for i in range(abs(dx)):
-                            if pi < 0:
-                                pi = pi + 2 * dy
-                            else:
-                                pi = pi + 2 * dy - 2 * dx
-                                y1 += 1
-                            x1 += 1
-                            self.drawPixel((x1, y1), color)
-                    # Octant 2
-                    elif 1 < slope and slope < math.inf:
-                        self.drawPixel((x1, y1), color)
-                        pi = 2 * dx - dy
-                        for i in range(abs(dy)):
-                            if pi < 0:
-                                pi = pi + 2 * dx
-                            else:
-                                pi = pi + 2 * dx - 2 * dy
-                                x1 += 1
-                            y1 += 1
-                            self.drawPixel((x1, y1), color)
-                # Q4
-                else:
-                    # Octant 7
-                    if -1 > slope and slope > -math.inf:
-                        self.drawPixel((x1, y1), color)
-                        p = 2 * dx - (-dy)
-                        for i in range(abs(-dy)):
-                            if p < 0:
-                                p = p + 2 * dx
-                            else:
-                                p = p + 2 * dx - 2 * (-dy)
-                                x1 += 1
-                            y1 -= 1
-                            self.drawPixel((x1, y1), color)
-                    # Octant 8
-                    elif 0 >= slope and slope >= -1:
-                        self.drawPixel((x1, y1), color)
-                        p = 2 * (-dy) - dx
-                        for i in range(abs(dx)):
-                            if p < 0:
-                                p = p + 2 * (-dy)
-                            else:
-                                p = p + 2 * (-dy) - 2 * dx
-                                y1 -= 1
-                            x1 += 1
-                            self.drawPixel((x1, y1), color)
-            # x < 0
-            elif x1 > x2:
-                # Q2
-                if y1 < y2:
-                    # Octant 3
-                    if -1 > slope and slope > -math.inf:
-                        self.drawPixel((x1, y1), color)
-                        p = 2 * (-dx) - dy
-                        for i in range(abs(dy)):
-                            if p < 0:
-                                p = p + 2 * (-dx)
-                            else:
-                                p = p + 2 * (-dx) - 2 * dy
-                                x1 -= 1
-                            y1 += 1
-                            self.drawPixel((x1, y1), color)
-                    # Octant 4
-                    elif 0 >= slope and slope >= -1:
-                        self.drawPixel((x1, y1), color)
-                        p = 2 * dy - (-dx)
-                        for i in range(abs(dx)):
-                            if p < 0:
-                                p = p + 2 * dy
-                            else:
-                                p = p + 2 * dy - 2 * (-dx)
-                                y1 += 1
-                            x1 -= 1
-                            self.drawPixel((x1, y1), color)
-                # Q3
-                else:
-                    # Octant 5
-                    if 0 <= slope and slope <= 1:
-                        self.drawPixel((x1, y1), color)
-                        p = 2 * (-dy) - (-dx)
-                        for i in range(abs(dx)):
-                            if p < 0:
-                                p = p + 2 * (-dy)
-                            else:
-                                p = p + 2 * (-dy) - 2 * (-dx)
-                                y1 -= 1
-                            x1 -= 1
-                            self.drawPixel((x1, y1), color)
-                    # Octant 6
-                    elif 1 < slope and slope < math.inf:
-                        self.drawPixel((x1, y1), color)
-                        p = 2 * (-dx) - (-dy)
-                        for i in range(abs(-dy)):
-                            if p < 0:
-                                p = p + 2 * (-dx)
-                            else:
-                                p = p + 2 * (-dx) - 2 * (-dy)
-                                x1 -= 1
-                            y1 -= 1
-                            self.drawPixel((x1, y1), color)
-            
+            if x1 < x2 and y1 < y2 and 0 <= slope and slope <= 1: bresenham_odd (x1, y1, dx, dy, 1, 1)                  # Octant 1
+            elif x1 < x2 and y1 < y2 and 1 < slope and slope < math.inf: bresenham_even (x1, y1, dx, dy, 1, 1)          # Octant 2
+            elif x1 > x2 and y1 < y2 and -1 > slope and slope > -math.inf: bresenham_even (x1, y1, -dx, dy, -1, 1)      # Octant 3
+            elif x1 > x2 and y1 < y2 and 0 >= slope and slope >= -1: bresenham_odd (x1, y1, -dx, dy, -1, 1)             # Octant 4
+            elif x1 > x2 and y1 > y2 and 0 <= slope and slope <= 1: bresenham_odd (x1, y1, -dx, -dy, -1, -1)            # Octant 5
+            elif x1 > x2 and y1 > y2 and 1 < slope and slope < math.inf: bresenham_even (x1, y1, -dx, -dy, -1, -1)      # Octant 6
+            elif x1 < x2 and y1 > y2 and -1 > slope and slope > -math.inf: bresenham_even (x1, y1, dx, -dy, 1, -1)      # Octant 7
+            elif x1 < x2 and y1 > y2 and 0 >= slope and slope >= -1: bresenham_odd (x1, y1, dx, -dy, 1, -1)             # Octant 8
